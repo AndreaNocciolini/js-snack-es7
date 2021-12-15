105b00813f7b233f325de06d1b12cf32996408b5
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-12-15 12:35:22 +0100

    start

---
+++ script.js
@@ -0,0 +1,7 @@
+// Partendo da questo array e aggiungiamo a  tutti gli elementi la proprieta age aggiungendo un numero casuale da 3 a 50.
+const arrayObj = [
+  {name: 'Poppy', type: 'tshirt', color: 'red'},
+  {name: 'Jumping', type: 'occhiali', color: 'blue'},
+  {name: 'CrissCross', type: 'scarpe', color: 'black'},
+  {name: 'Jenny', type: 'borsa', color: 'pink'},
+];
