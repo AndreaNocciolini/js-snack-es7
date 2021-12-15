b37d51b218cec2db0d48e89e9a1b311314463b52
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-12-15 12:48:17 +0100

    miscellaneous

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-// Partendo da questo array e aggiungiamo a  tutti gli elementi la proprieta age aggiungendo un numero casuale da 3 a 50.
+// Partendo da questo array, aggiungiamo a  tutti gli elementi la proprieta age aggiungendo un numero casuale da 3 a 50.
 const arrayObj = [
   {name: 'Poppy', type: 'tshirt', color: 'red'},
   {name: 'Jumping', type: 'occhiali', color: 'blue'},
   {name: 'CrissCross', type: 'scarpe', color: 'black'},
   {name: 'Jenny', type: 'borsa', color: 'pink'},
 ];
+
+for (let i = 0; i < arrayObj.length; i++) {
+    const element = arrayObj[i];
+    element.age = Math.floor(Math.random() * 50) + 3;
+    document.getElementById(`container`).innerHTML += `
+        <div class="red">Name: ${element.name}</div>
+        <div class="blue">Type: ${element.type}</div>
+        <div>Color: ${element.color}</div>
+        <div class="green">Age: ${element.age}</div>
+    `
+}
+console.log(arrayObj)
+
